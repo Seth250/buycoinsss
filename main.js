34d51a118337fa959dd73c9e1e08eef846156524
Seth250 <ayowaleakintayo@gmail.com>
2020-11-26 23:13:30 +0100

    finished responsive header styles

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-window.addEventListener('load', () => console.log('loaded lol'), false);
+function initialize(){
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+    hamburger.addEventListener('click', () => menu.classList.toggle('item-collapse-mobile'), false);
+}
+
+window.addEventListener('load', initialize, false);
